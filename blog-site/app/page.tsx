70c333ee07e9db3c2f97494804ba31ea138f6c9b
Christopher Waschke <chrisdwaschke@gmail.com>
2025-06-08 14:06:38 -0700

    Started adding posting modal.

--- blog-site/app/page.tsx
+++ blog-site/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Gemma.</h1>
+      <h1 className="prose prose-2xl">Gemma.</h1>
     </main>
   );
 }
--- blog-site/components/client/NavBar.tsx
+++ blog-site/components/client/NavBar.tsx
@@ -2,23 +2,48 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { NotebookPen } from "lucide-react";
+import { SendHorizontal } from "lucide-react";
+import React, {useEffect, useState} from "react";
 
-const Editor = () => {
-  const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Gemma.</p>",
-  });
-  return <EditorContent editor={editor} />;
-};
+
 
 export default function NavBar() {
+  const [text, setText] = useState<string>("");
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Write your post here...</p>",
+    immediatelyRender: false,
+    editorProps: {
+      attributes: {
+        class: "prose border rounded bg-base-100",
+      }
+    }
+  });
+  
+  useEffect(() => {
+    if(text.length > 0) {
+      console.log("Text set as value:", text);
+    }
+  }, [text])
+
   return (
     <>
       <div className="flex bg-base-200 py-2 px-4 items-center justify-between">
-        <dialog className="modal" id="post-modal">
+        <dialog className="modal" id="post-modal" onAnimationEnd={e => console.log("Animation Ended.")} onClose={() => {editor?.commands.setContent("<p>Write your post here...</p>")}}>
           <div className="modal-box">
-            <button className="btn btn-primary">Gemma.</button>
-            <Editor />
+            <EditorContent editor={editor} />
+            <div className="modal-action">
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  const dialog = document.getElementById("post-modal") as HTMLDialogElement;
+                  if (dialog) {
+                    dialog.close();
+                  }
+                  setText(editor?.getText() || "");
+                }}
+                ><SendHorizontal /> Post</button>
+            </div>
           </div>
         </dialog>
         <h1 className="text-2xl font-bold select-none">Blog Site</h1>
