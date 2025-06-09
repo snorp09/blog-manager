28214666abe37ac23f2ebbe7a136221ba490fd2e
Christopher Waschke <chrisdwaschke@gmail.com>
2025-06-09 14:57:08 -0700

    Remove temporary changes for developing Post Editor.

--- blog-site/app/layout.tsx
+++ blog-site/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen flex flex-col">
+      <body>
         <NavBar />
         {children}
       </body>
