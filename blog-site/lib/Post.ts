b4aed90837b1e372bb4d2692c17920499f7cc035
Christopher Waschke <chrisdwaschke@gmail.com>
2025-06-09 15:44:18 -0700

    Added implementation to sendPost.

--- blog-site/lib/Post.ts
+++ blog-site/lib/Post.ts
@@ -3,8 +3,18 @@ interface PostOutGoing {
     body: String
 }
 
-function sendPost(post: PostOutGoing){
-    console.error("Not Implemented.");
+async function sendPost(post: PostOutGoing){
+    const resp = await fetch("/api/posts/new", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(post)
+    });
+    if(resp.status != 200){
+        console.error("Error sending post.");
+        alert("Unable to send post.");
+    }
 }
 
 export type {PostOutGoing};
